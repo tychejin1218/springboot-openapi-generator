910b1d5b1edd891375520be9e7ab35162e3a972e
너나들이 <tychejin@naver.com>
2024-03-20 09:57:00 +0900

    Feat: OpenApi Generator 설정

---
+++ src/main/java/com/example/openapi/SpringbootOpenapiGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.openapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOpenapiGeneratorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootOpenapiGeneratorApplication.class, args);
+  }
+
+}
